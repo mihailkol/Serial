6d6d10bafe341ace564e49fe037c9b16cf60a30d
mihailkol <m891391584656@gmail.com>
2024-09-09 09:52:09 +0700

    Добавьте файлы проекта.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Serial
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.cboxComport = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tboxReceive = new System.Windows.Forms.TextBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tboxData = new System.Windows.Forms.TextBox();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.labelCurrentData = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboxComport
+            // 
+            this.cboxComport.FormattingEnabled = true;
+            this.cboxComport.Location = new System.Drawing.Point(77, 11);
+            this.cboxComport.Name = "cboxComport";
+            this.cboxComport.Size = new System.Drawing.Size(121, 21);
+            this.cboxComport.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(203, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(72, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // tboxReceive
+            // 
+            this.tboxReceive.Location = new System.Drawing.Point(11, 92);
+            this.tboxReceive.Multiline = true;
+            this.tboxReceive.Name = "tboxReceive";
+            this.tboxReceive.ReadOnly = true;
+            this.tboxReceive.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tboxReceive.Size = new System.Drawing.Size(264, 339);
+            this.tboxReceive.TabIndex = 6;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(11, 10);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(60, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(200, 466);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tboxData
+            // 
+            this.tboxData.Location = new System.Drawing.Point(12, 466);
+            this.tboxData.Multiline = true;
+            this.tboxData.Name = "tboxData";
+            this.tboxData.Size = new System.Drawing.Size(182, 23);
+            this.tboxData.TabIndex = 5;
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(12, 41);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(75, 23);
+            this.btnUp.TabIndex = 7;
+            this.btnUp.Text = "Up";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(107, 41);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(75, 23);
+            this.btnDown.TabIndex = 8;
+            this.btnDown.Text = "Down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(200, 41);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(200, 437);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 10;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(11, 436);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(183, 23);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // labelCurrentData
+            // 
+            this.labelCurrentData.AutoSize = true;
+            this.labelCurrentData.Location = new System.Drawing.Point(13, 71);
+            this.labelCurrentData.Name = "labelCurrentData";
+            this.labelCurrentData.Size = new System.Drawing.Size(64, 13);
+            this.labelCurrentData.TabIndex = 12;
+            this.labelCurrentData.Text = "CurrentData";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.chart1.Location = new System.Drawing.Point(292, 10);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(884, 479);
+            this.chart1.TabIndex = 13;
+            this.chart1.Text = "chart1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1188, 534);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.labelCurrentData);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.tboxReceive);
+            this.Controls.Add(this.tboxData);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cboxComport);
+            this.Name = "MainForm";
+            this.Text = "Serial V1.0";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboxComport;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tboxReceive;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tboxData;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label labelCurrentData;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Serial
+{
+    public partial class MainForm : Form
+    {
+
+        private SerialPort Serial = new SerialPort();
+        //public int[] wghtlng = new int[2];
+        List<long> x;
+        List<long> y;
+
+        #region Local Helpers
+        private void UpdateCOMPortList()
+        {
+            // Get all existing Com Port names
+            string[] Ports = System.IO.Ports.SerialPort.GetPortNames();
+            cboxComport.Items.Clear();
+
+            // Append existing COM to the cboxComport list
+            foreach (var item in Ports)
+            {
+                cboxComport.Items.Add(item);
+            }
+        }
+        #endregion
+
+        #region Delegates
+        public delegate void UPDATE_OUTPUT_TEXT(String Str);
+        public void UpdateOutputText(String Str)
+        {
+            string[] subs = Str.Split(',');
+            tboxReceive.Text = Str + tboxReceive.Text;
+            tboxReceive.ScrollToCaret();
+            try
+            {
+                long strenghth = long.Parse(subs[0]);
+                long length = long.Parse(subs[1]);
+
+
+            labelCurrentData.Text = "усилие " + strenghth + ", расстояние " + length;
+            chart1.Series["Series1"].LegendText = "График XY";
+
+            x = new List<long>();
+            y = new List<long>();
+
+            x.Add(length);
+            y.Add(strenghth);
+            }
+            catch { }
+
+            for (int i = 0; i < x.Count; i++)
+            {
+                chart1.Series["Series1"].Points.AddXY(x[i], y[i]);
+            }
+            chart1.Series["Series1"].LegendText = "График XY";
+            for (int i = 0; i < x.Count; i++)
+            {
+                chart1.Series["Series1"].Points.AddXY(x[i], y[i]);
+            }
+        }
+        #endregion
+
+        #region Handlers
+        void SerialOnReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            String str = Serial.ReadExisting();
+            
+            Invoke(new UPDATE_OUTPUT_TEXT(UpdateOutputText), str);
+        }
+        #endregion
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // If user click disconnect
+            if ("Disconnect" == btnConnect.Text.ToString())
+            {
+                if (true == Serial.IsOpen)
+                {
+                    Serial.Close();
+                }
+
+                btnConnect.Text = "Connect";
+                cboxComport.Enabled = true;
+                //cboxBaudrate.Enabled = true;
+                btnRefresh.Enabled = true;
+
+                return;
+            }
+
+            // else we gonna open the desired COM port
+            // Get user comport from cbox
+            try
+            {
+                Serial.PortName = cboxComport.Text;
+            }
+            catch
+            {
+                MessageBox.Show("Error! No COM Port selected");
+                return;
+            }
+
+            // Serial Port Configuration
+            Serial.BaudRate = 115200;
+            Serial.Parity = Parity.None;
+            Serial.DataBits = 8;
+            Serial.ReceivedBytesThreshold = 1;
+            Serial.StopBits = StopBits.One;
+            Serial.Handshake = Handshake.None;
+            Serial.WriteTimeout = 3000;
+
+            // Check if com port is opened by other application
+            if (false == Serial.IsOpen)
+            {
+                try
+                {
+                    // Com port available
+                    Serial.Open();
+                }
+                catch
+                {
+                    MessageBox.Show("The COM port is not accessible", "Error");
+                    return;
+                };
+
+
+                // double comform it is opened
+                if (true == Serial.IsOpen)
+                {
+                    btnConnect.Text = "Disconnect";
+                    cboxComport.Enabled = false;
+                    //cboxBaudrate.Enabled = false;
+                    btnRefresh.Enabled = false;
+
+                    // Add callback handler for receiving
+                    Serial.DataReceived += new SerialDataReceivedEventHandler(SerialOnReceivedHandler);
+
+                }
+
+            }
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // We need to populate the lists during mainform is loading
+            UpdateCOMPortList();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            // We need to update all lists again if user requested
+            UpdateCOMPortList();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if(null != Serial)
+            {
+                if(true == Serial.IsOpen)
+                {
+                    Serial.Write(tboxData.Text);
+                }
+                else
+                {
+                    MessageBox.Show("COM Port is not Opened");
+                }
+            }
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            if (null != Serial)
+            {
+                if (true == Serial.IsOpen)
+                {
+                    Serial.Write("1");
+                }
+                else
+                {
+                    MessageBox.Show("COM Port is not Opened");
+                }
+            }
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            if (null != Serial)
+            {
+                if (true == Serial.IsOpen)
+                {
+                    Serial.Write("0");
+                }
+                else
+                {
+                    MessageBox.Show("COM Port is not Opened");
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (null != Serial)
+            {
+                if (true == Serial.IsOpen)
+                {
+                    Serial.Write("s");
+                }
+                else
+                {
+                    MessageBox.Show("COM Port is not Opened");
+                }
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            {
+                //Создаем поток для записи в файл и загружаем в него файл
+                //при отсутствии файла он будет создан
+                string fileName = DateTime.Now.ToString("yyyy.MM.dd hh.mm") + ".csv";
+                FileStream filestream =
+                  new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
+                //Очищаем поток
+                filestream.SetLength(0);
+                //StreamWriter создаем для потока filestream
+                StreamWriter streamwriter = new StreamWriter(filestream);
+                //Записываем текст, введенный в textBox1 в файл
+                streamwriter.Write(tboxReceive.Text);
+                //Освобождаем ресурсы
+                streamwriter.Flush();
+                streamwriter.Close();
+                filestream.Close();
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            tboxReceive.Text = "";
+            chart1.Series["Series1"].Points.Clear();
+        }
+    }
+}
